7fffd75eecf24ad6d781dabb21667098778337b5
pmbittner <paul.bittner@uni-ulm.de>
2024-09-12 16:23:07 +0200

    fix: extra ; in package header
    According to an example for a package header by hlissner here:
    https://github.com/doomemacs/doomemacs/issues/574#issuecomment-389270764
    the package header should have only double ; in these places.

--- everforest.el
+++ everforest.el
@@ -2,10 +2,10 @@
 ;;
 ;; Copyright (C) 2022 Evan Sarris
 ;;
-;;;  Author: Theory_of_Everything <evan@theoryware.net>
-;;;  Version: 0.1
-;;;  Homepage: https://git.sr.ht/~theorytoe/everforest-theme
-;;;  Package-Requires: ((emacs "24"))
+;;  Author: Theory_of_Everything <evan@theoryware.net>
+;;  Version: 0.1
+;;  Homepage: https://git.sr.ht/~theorytoe/everforest-theme
+;;  Package-Requires: ((emacs "24"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
